dcbb54a2a550cdb4394dd76a5a84f4582cf1a0e0
MihaGoHard <shasherin06@bk.ru>
2022-10-11 19:09:25 +0300

    правки функций

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
-function isPosNumber(num) {
-  return !isNaN(num) && typeof num === 'number' && num >= 0;
-}
+const isValidNumber = (num) => (typeof num === 'number' && num >= 0) ;
 
-function isString(str) {
-  return typeof str === 'string';
-}
+const getRandFromRange = (min, max) => {
+  const minIsValid = isValidNumber(min);
+  const maxIsValid = isValidNumber(max);
 
-function getRandFromRange(min, max) {
-
-  if (isPosNumber(min) && isPosNumber(max)) {
+  if (minIsValid && maxIsValid) {
     const index = (min === max ? 0 : 1);
     return Math.round(Math.random() * (max - min + index) + min);
   }
 
   return NaN;
-}
+};
 
-getRandFromRange(0, -10);
+getRandFromRange(30, 10);
 
 
-function stringIsLimited(inputStr, lengthLimit) {
+const stringIsLimited = (inputStr, lengthLimit) => {
 
-  if (isString(inputStr) && isPosNumber(lengthLimit) && (inputStr.length <= lengthLimit)) {
+  const strIsValid = ((typeof str === 'string') && (inputStr.length <= lengthLimit));
+  const lengthLimitIsValid = isValidNumber(lengthLimit);
+
+  if (strIsValid && lengthLimitIsValid) {
     return true;
   }
 
   return false;
-}
+};
 
 stringIsLimited('', 0);
