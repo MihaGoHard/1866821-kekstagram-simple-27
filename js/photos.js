f2f8a25d85687a65b830b66290bf3cf38900e3e0
MihaGoHard <shasherin06@bk.ru>
2022-10-16 08:47:15 +0300

    отктил предыдущие изменения, разобрался

--- js/photos.js
+++ js/photos.js
@@ -9,9 +9,7 @@ const createPhotoElem = (dataPhoto) => {
 
 const fillPhotos = (dataPhotos) => {
   const domPhotos = document.querySelector('.pictures');
-  domPhotos.append(...dataPhotos.map((dataPhoto) => {
-    createPhotoElem(dataPhoto);
-  }));
+  domPhotos.append(...dataPhotos.map((dataPhoto) => createPhotoElem(dataPhoto)));
 };
 
 export {fillPhotos};
